c9e1b10a73bf958cec13712357806f155c249196
icbestuer <icbestuer@gmail.com>
2014-05-02 20:22:44 +0200

    primer commit

---
+++ Assets/CameraController.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+public var player : GameObject;
+private var offset : Vector3;
+
+function Start () {
+	offset = transform.position;
+}
+
+function Update () {
+	transform.position = player.transform.position + offset;
+}
---
+++ Assets/Scripts/PlayerController.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+public var speed : float ;
+
+function FixedUpdate () {
+	var moveHorizontal = Input.GetAxis("Horizontal");
+	var moveVertical = Input.GetAxis("Vertical");
+	
+	var movement = new Vector3(moveHorizontal, 0.0, moveVertical);
+	
+	rigidbody.AddForce(movement * speed * Time.deltaTime);
+}
