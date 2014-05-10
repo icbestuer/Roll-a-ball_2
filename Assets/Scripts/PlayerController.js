373bc7633e375e96b9b506af72b338ecf0218a6c
icbestuer <icbestuer@gmail.com>
2014-05-10 18:34:34 +0200

    terminado
    terminado

--- Assets/Scripts/PlayerController.js
+++ Assets/Scripts/PlayerController.js
@@ -1,6 +1,15 @@
 ﻿#pragma strict
 
 public var speed : float ;
+public var countText : GUIText;
+public var winText : GUIText;
+private var count : int;
+
+function Start(){
+	count = 0;
+	SetCountText();
+	winText.text = "";
+}
 
 function FixedUpdate () {
 	var moveHorizontal = Input.GetAxis("Horizontal");
@@ -9,4 +18,19 @@ function FixedUpdate () {
 	var movement = new Vector3(moveHorizontal, 0.0, moveVertical);
 	
 	rigidbody.AddForce(movement * speed * Time.deltaTime);
+}
+
+function OnTriggerEnter(other : Collider){
+	if(other.gameObject.tag == "PickUp"){
+		other.gameObject.SetActive(false);
+		count = count + 1;
+		SetCountText();
+	}
+}
+
+function SetCountText(){
+	countText.text = "Count: " + count;
+	if(count >= 13){
+		winText.text = "YOU WIN !!";
+	}
 }
